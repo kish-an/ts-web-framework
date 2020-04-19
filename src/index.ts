bc2afbe1f49d784293dfc4a7824ae8754b28e68f
Kishan <kishan.o@hotmail.com>
2020-04-19 16:58:48 +0100

    Implement User model

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Hello 👋');
+import { User } from './models/User';
+
+const user = new User({ name: 'John', age: 20 });
+
+user.set({ name: 'Jack' });
+
+console.log(user.get('age'));
+console.log(user.get('name'));
