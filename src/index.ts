8e20119175fef0fc86a413d088fd39a47e3948fe
Kishan <kishan.o@hotmail.com>
2020-04-26 20:01:10 +0100

    Add JSON server and save + fetch functionality

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import { User } from './models/User';
+/* import { User } from './models/User';
+import axios from 'axios';
 
 const user = new User({ name: 'John', age: 20 });
 
@@ -14,7 +15,16 @@ user.on('change', () => {
     console.log('Changed!');
 });
 
-
 console.log(user);
 
 user.trigger('click');
+
+axios.get('http://localhost:3000/users/1')
+    .then(res => console.log(res.data));
+ */
+
+import { User } from './models/User';
+
+const user = new User({ name: 'Amy', age: 25 });
+
+user.save();
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,7 @@
+import axios, { AxiosResponse } from 'axios';
+
 interface UserProps {
+    id?: number;
     name?: string;
     age?: number;
 }
@@ -39,5 +42,22 @@ export class User {
         handlers.forEach(callback => {
             callback();
         });
+    }
+
+    fetch(): void {
+        axios.get(`http://localhost:3000/users/${this.get('id')}`)
+            .then((res: AxiosResponse): void => {
+                this.set(res.data);
+            })
+    }
+
+    save(): void {
+        const id = this.get('id');
+
+        if (id) {
+            axios.put(`http://localhost:3000/users/${id}`, this.data);
+        } else {
+            axios.post('http://localhost:3000/users', this.data);
+        }
     }
 }
