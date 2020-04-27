7c0aed290afe29004b94852f909825f8f4a0d4a5
Kishan <kishan.o@hotmail.com>
2020-04-27 16:47:10 +0100

    Refactor with composition

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,10 @@
-/* import { User } from './models/User';
-import axios from 'axios';
-
-const user = new User({ name: 'John', age: 20 });
-
-user.on('click', () => {
-    console.log('Hello 👋');
-});
-
-user.on('click', () => {
-    console.log('Clicked!');
-});
-
-user.on('change', () => {
-    console.log('Changed!');
-});
-
-console.log(user);
-
-user.trigger('click');
-
-axios.get('http://localhost:3000/users/1')
-    .then(res => console.log(res.data));
- */
-
 import { User } from './models/User';
 
-const user = new User({ name: 'Amy', age: 25 });
+const user = new User({ name: 'Jack', age: 19 });
 
-user.save();
+const userProps = {
+    name: user.attributes.get('name'),
+    age: user.attributes.get('age'),
+}
+
+user.sync.save(userProps);
